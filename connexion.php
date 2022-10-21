a42d83b3b42aace077f6cac0866705844b3c1745
tgabor1 <t.gabor@tutanota.com>
2022-10-21 22:24:41 +0200

    _

--- connexion.php
+++ connexion.php
@@ -5,7 +5,7 @@
 // Test de connexion
 try
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=mini_chat;charset=utf8', 'root', ''); // attention un mot de passe a été défini pour l'accès à la base de données
+    $bdd = new PDO('mysql:host=localhost;dbname=PHP-Minichat;charset=utf8', 'root', ''); // attention un mot de passe a été défini pour l'accès à la base de données
 }
 
 // Gestion des erreurs
